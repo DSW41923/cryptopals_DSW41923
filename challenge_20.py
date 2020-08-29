08ea9bba1f5aacc372e2aa7ad924d99a42a1dd91
DSW41923 <dsw41923@gmail.com>
2020-08-28 22:58:08 -0400

    Completing challenge 18 to 22

---
+++ challenge_20.py
@@ -0,0 +1,86 @@
+import sys
+import getopt
+import binascii
+import base64
+import secrets
+
+from challenge_02 import bytestrxor
+from challenge_03 import evaluate_palintext
+from challenge_19 import fixed_nonce_CTR_crtyptor
+
+
+def main(argv):
+
+    try:
+        opts, args = getopt.getopt(argv,"h:",["help"])
+    except getopt.GetoptError:
+        print('Usage: python3 challenge_20.py [-h | --help]')
+        sys.exit(2)
+
+    for opt, arg in opts:
+        if opt in ('-h', "--help"):
+            print('Usage: python3 challenge_20.py [-h | --help]')
+            print('Challenge 20: Break fixed-nonce CTR statistically')
+            sys.exit()
+
+    plaintexts = []
+    ciphertexts = []
+    for line in open('input_20.txt', 'r'):
+        plaintext = base64.b64decode(line.encode())
+        plaintexts.append(plaintext)
+        ciphertexts.append(fixed_nonce_CTR_crtyptor(plaintext))
+
+    # Consider bytes at same position in different ciphertext as result of single-char XOR
+    separated_ciphertext_by_bytes = [b''] * max([len(c) for c in ciphertexts])
+    for ciphertext in ciphertexts:
+        for byte_num, byte in enumerate(ciphertext):
+            separated_ciphertext_by_bytes[byte_num] += byte.to_bytes(1, 'big')
+
+    key_byte_candidates = []
+    for byted_ct in separated_ciphertext_by_bytes:
+        if len(byted_ct) < len(ciphertexts):
+            continue
+        best_score = 0
+        key_candidate = b''
+        for y in range(256):
+            trial_byte = y.to_bytes(1, 'big')
+            possible_key_bytes = trial_byte * len(byted_ct)
+            possible_pt = bytestrxor(byted_ct, possible_key_bytes)
+            trial_score = evaluate_palintext(possible_pt)
+            if trial_score >= best_score:
+                best_score = trial_score
+                key_candidate += trial_byte
+        key_byte_candidates.append(key_candidate)
+
+    # Reducing candidates by choosing the last candidate for each byte as beginning
+    temporary_key = b''
+    for byte in key_byte_candidates:
+        key_byte = byte[-1].to_bytes(1, 'big')
+        temporary_key += key_byte
+
+    # Keep the fixing part here to fix some byte if desired
+    while True:
+        current_plaintexts = b"\n".join([bytestrxor(temporary_key, c) for c in ciphertexts])
+        print("Current decrypt result is: ")
+        print(current_plaintexts.decode())
+        to_fix = input("Input byte number to fix key or N/n to end fixing: ")
+        if to_fix in ["N", "n"]:
+            break
+        else:
+            try:
+                byte_num_to_fix = int(to_fix)
+                print("Candidates of key bytes for byte " + to_fix + " is:")
+                byte_candidates = list(map(int, key_byte_candidates[byte_num_to_fix]))
+                print(byte_candidates)
+                print("Current key bytes for byte " + to_fix + " is: " + str(temporary_key[byte_num_to_fix]))
+                byte_selection = int(input("Enter a new byte: ")).to_bytes(1, 'big')
+                temporary_key = temporary_key[:byte_num_to_fix] + byte_selection + temporary_key[byte_num_to_fix + 1:]
+            except TypeError:
+                continue
+
+    final_plaintexts = b"\n".join([bytestrxor(temporary_key, c) for c in ciphertexts])
+    print("Final decrypt result is: ")
+    print(final_plaintexts.decode())
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
---
+++ challenge_21.py
@@ -0,0 +1,101 @@
+import sys
+import getopt
+import binascii
+import base64
+import secrets
+import time
+
+
+class MT19937_RNG(object):
+    """Implementing MT19937 Mersenne Twister RNG"""
+    # Parameters of MT19937
+    w, n, m, r = 32, 624, 397, 31
+    a = 0x9908B0DF
+    u, d = 11, 0xFFFFFFFF
+    s, b = 7, 0x9D2C5680
+    t, c = 15, 0xEFC60000
+    f = 1812433253
+    l = 18
+    def __init__(self, seed):
+        super(MT19937_RNG, self).__init__()
+        self.state = []
+        self.count = 0
+        self.seed_mt(seed)
+        
+    def seed_mt(self, seed):
+        self.count = self.n
+        self.state.append(seed)
+        for i in range(1, self.n):
+            new_state = self.f * (self.state[i-1] ^ (self.state[i-1] >> (self.w-2))) + i
+            state_bit = bin(new_state)[2:].zfill(self.w)
+            self.state.append(int(state_bit[-self.w:], 2))
+
+    def extract_number(self):
+        if not self.state:
+            raise
+
+        if self.count == self.n:
+            self.twist()
+
+        y = self.state[self.count]
+        y = y ^ ((y >> self.u) & self.d)
+        y = y ^ ((y << self.s) & self.b)
+        y = y ^ ((y << self.t) & self.c)
+        y = y ^ (y >> self.l)
+        self.count += 1
+        return bin(y)[2:].zfill(self.w)[-self.w:]
+
+    # Generate the next n values from the series x_i 
+    def twist(self):
+        for i in range(self.n):
+            current_state_bit = bin(self.state[i])[2:].zfill(self.w)
+            next_state_bit = bin(self.state[(i + 1) % self.n])[2:].zfill(self.w)
+            x = int(current_state_bit[:(self.w - self.r)] + next_state_bit[-self.r:], 2)
+            xA = x >> 1
+            if (x % 2) != 0:
+                 xA = xA ^ self.a
+            self.state[i] = self.state[(i + self.m) % self.n] ^ xA
+
+        self.count = 0
+
+
+def main(argv):
+
+    try:
+        opts, args = getopt.getopt(argv,"h:",["help"])
+    except getopt.GetoptError:
+        print('Usage: python3 challenge_21.py [-h | --help]')
+        sys.exit(2)
+
+    for opt, arg in opts:
+        if opt in ('-h', "--help"):
+            print('Usage: python3 challenge_21.py [-h | --help]')
+            print('Challenge 21: Implement the MT19937 Mersenne Twister RNG')
+            sys.exit()
+
+    # Trying the MT19937 RNG
+    seed = secrets.choice(range(2 ** 32 - 1))
+    first_RNG = MT19937_RNG(seed)
+    outputs = []
+    for x in range(64):
+        number = first_RNG.extract_number()
+        outputs.append(number)
+
+    time.sleep(13)
+
+    new_seed = seed
+    second_RNG = MT19937_RNG(new_seed)
+    for x in range(64):
+        new_number = second_RNG.extract_number()
+        if outputs[x] != new_number:
+            print(seed, new_seed)
+            print(outputs[x], new_number)
+            print(x)
+            import pdb; pdb.set_trace()
+            print("Incorrect implementaion!")
+            sys.exit(2)
+    print("Correct implementaion! Good Job!")
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
